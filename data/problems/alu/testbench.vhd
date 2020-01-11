6b9ee0e8ab2960fca36bbee5781a222f9b317026
Steven Bell <botsnlinux@gmail.com>
2020-01-10 19:50:31 -0500

    Update first batch of problems

--- data/problems/alu/testbench.vhd
+++ data/problems/alu/testbench.vhd
@@ -29,51 +29,64 @@ begin
   dut : alu port map(a, b, s, y);
 
   process
-    variable l : line; -- For output text
+    variable errors : integer := 0;
+
+    -- Standard check function
+    procedure check(condition : boolean; message : string) is
+    begin
+      if not condition then
+        report message;
+        errors := errors + 1;
+      end if;
+    end check;
+
   begin
     a <= x"0";
     b <= x"7";
     s <= "00"; wait for 10 ns;
-    assert(y = x"0") report "test failed for 0 AND 7.";
+    check(y = x"0", "test failed for 0 AND 7.");
 
     a <= x"f";
     b <= x"a";
     s <= "00"; wait for 10 ns;
-    assert(y = x"a") report "test failed for xF AND xA.";
+    check(y = x"a", "test failed for xF AND xA.");
 
     a <= x"3";
     b <= x"c";
     s <= "01"; wait for 10 ns;
-    assert(y = x"f") report "test failed for 3 OR 0xC.";
+    check(y = x"f", "test failed for 3 OR 0xC.");
 
     a <= x"1";
     b <= x"2";
     s <= "01"; wait for 10 ns;
-    assert(y = x"3") report "test failed for 1 OR 2.";
+    check(y = x"3", "test failed for 1 OR 2.");
 
     a <= x"2";
     b <= x"2";
     s <= "10"; wait for 10 ns;
-    assert(y = x"4") report "test failed for 2 + 2.";
+    check(y = x"4", "test failed for 2 + 2.");
 
     a <= x"f";
     b <= x"2";
     s <= "10"; wait for 10 ns;
-    assert(y = x"1") report "test failed for xF + 2.";
+    check(y = x"1", "test failed for xF + 2.");
 
     a <= x"e";
     b <= x"2";
     s <= "11"; wait for 10 ns;
-    assert(y = x"c") report "test failed for xE - 2.";
+    check(y = x"c", "test failed for xE - 2.");
 
     a <= x"0";
     b <= x"1";
     s <= "11"; wait for 10 ns;
-    assert(y = x"f") report "test failed for 0 - 1.";
+    check(y = x"f", "test failed for 0 - 1.");
 
-
-    write (l, String'("Test complete."));
-    writeline (output, l);
+    if errors = 0 then
+      write (output, "TEST PASSED." & LF);
+    else
+      write (output, "Test failed with " & to_string(errors) &  " errors." & LF);
+    end if;
+ 
     wait;
   end process;
 end test;
