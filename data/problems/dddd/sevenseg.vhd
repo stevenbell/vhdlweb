3526ccd40ea28fd910313ef846e805445e349c12
Steven Bell <botsnlinux@gmail.com>
2024-10-22 11:38:46 -0400

    hide metavalue warnings on dddd

--- data/problems/dddd/sevenseg.vhd
+++ data/problems/dddd/sevenseg.vhd
@@ -10,22 +10,24 @@ entity sevenseg is
 end sevenseg;
 
 architecture synth of sevenseg is
+  signal sl : std_logic_vector(3 downto 0);
 begin
-  segments <= "1111110" when S = 4x"0" else
-              "0110000" when S = 4x"1" else
-              "1101101" when S = 4x"2" else
-              "1111001" when S = 4x"3" else
-              "0110011" when S = 4x"4" else
-              "1011011" when S = 4x"5" else
-              "1011111" when S = 4x"6" else
-              "1110000" when S = 4x"7" else
-              "1111111" when S = 4x"8" else
-              "1110011" when S = 4x"9" else
-              "1110111" when S = 4x"a" else
-              "0011111" when S = 4x"b" else
-              "1001110" when S = 4x"c" else
-              "0111101" when S = 4x"d" else
-              "1001111" when S = 4x"e" else
+  sl <= std_logic_vector(S); -- Convert to std_logic_vector to avoid the metavalue warnings at t=0
+  segments <= "1111110" when sl = 4x"0" else
+              "0110000" when sl = 4x"1" else
+              "1101101" when sl = 4x"2" else
+              "1111001" when sl = 4x"3" else
+              "0110011" when sl = 4x"4" else
+              "1011011" when sl = 4x"5" else
+              "1011111" when sl = 4x"6" else
+              "1110000" when sl = 4x"7" else
+              "1111111" when sl = 4x"8" else
+              "1110011" when sl = 4x"9" else
+              "1110111" when sl = 4x"a" else
+              "0011111" when sl = 4x"b" else
+              "1001110" when sl = 4x"c" else
+              "0111101" when sl = 4x"d" else
+              "1001111" when sl = 4x"e" else
               "1000111";
 end;
 
